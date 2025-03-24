3f8ac16191b63e5e7cf9d2b15c0a6b1af0b9372b
chimae <chimandukwe.r@gmail.com>
2025-03-24 17:07:26 +0000

    Reduced the size of the logo

--- components/Home/Nav.tsx
+++ components/Home/Nav.tsx
@@ -11,7 +11,7 @@ const Nav = () => {
     <div className="h-[12vh] sticky top-0 z-[1] bg-rose-600 shadow-md">
       <div className="flex items-center justify-between w-[95%] md-w-4/5 mx-auto h-full">
         {/* Logo */}
-        <Link href="/" className="text-4xl text-white font-bold">
+        <Link href="/" className="md:text-4xl text-2xl  text-white font-bold">
         E-shop
         </Link>
 
